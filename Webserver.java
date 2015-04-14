d0d506726a1193b6212e942bdcae537312617409
Werenne <aurelien.werenne@hotmail.com>
2015-04-14 21:58:30 +0200

    First version of Request class
    First version finished
    Commented
    Not tested

--- Webserver.java
+++ Webserver.java
@@ -183,24 +183,24 @@ class Request
 
 
 
-	/* -- Returns argument value, null if argument name is not set -- */ 
-	public Map<String,String> get_argument(String argumentName) 
+	/* -- Returns argument value, empty if argument name is not set -- */ 
+	public String get_argument(String argumentName) 
 	{ 
 		if (arguments.containsKey(argumentName))
 			return arguments.get(argumentName);
 		else 
-			return null;
+			return "";
 	}
 
 
 
-	/* -- Returns headerline value of key, null if name of headerline is not set -- */ 
-	public Map<String,String> get_headerline(String headerlineName) 
+	/* -- Returns headerline value of key, empty if name of headerline is not set -- */ 
+	public String get_headerline(String headerlineName) 
 	{ 
 		if (headerlines.containsKey(headerlineName))
 			return headerlines.get(headerlineName);
 		else 
-			return null; 
+			return ""; 
 	}
 
 
@@ -229,67 +229,120 @@ class Request
 
 
 		/* Extract url and eventual arguments */
-		request = request.substring(i);
-		i = request.indexOf("/");
-		request = request.substring(i);
-		i = request.indexOf(" "); 
+		request = request.substring(i); 	
+		i = request.indexOf("/"); 			// Take begin at '/'
+		request = request.substring(i); 
+		i = request.indexOf(" ");  			// Take end before next space
 		url = request.substring(0, i-1);
 
-		if (url.indexOf("?") != -1) 	// Extract arguments and clean url of it
+
+		if (url.indexOf("?") != -1) 	// Extract arguments and url
 		{
-			int j = url.indexOf("?");
+			int j = url.indexOf("?");  		// Take begin of args after '?'
 			String args = url.substring(j+1);
-			url = url.substring(0, j-1);
+			url = url.substring(0, j-1); 	// Extract url before '?'
 
 			while (true)
 			{
-				int k = args.indexOf("=");
+				int k = args.indexOf("="); 		// Take end of argumentname before '='
 				if (k == -1)
 				{
 					validRequest = false;
 					return;
 				}
 
-				int l = args.indexOf("&");
+				int l = args.indexOf("&"); 		// Take end of argumentvalue before '&'
 
 				if (l == -1) // If last argument of url
 				{
-					arguments.put(args.substring(0, k-1), args.substring(k+1));
+					if (l+1 >= args.length()) 	// Empty argument
+						break;
+
+					arguments.put(args.substring(0, k-1), args.substring(k+1)); 	// Put argname and argvalue in map
 					break;
 				}
 
-				arguments.put(args.substring(0, k-1), args.substring(k+1, l-1));
+				arguments.put(args.substring(0, k-1), args.substring(k+1, l-1)); 	// Put argname and argvalue in map
+				args = args.substring(l+1); 	// Go to next argument
 			} 
 		}
 
 
 		/* Extract http version */
+		request = request.substring(i); 
+		i = request.indexOf("H"); 		// Take begin at 'H'
 		request = request.substring(i);
-		i = request.indexOf("H");
-		request = request.substring(i);
-		i = request.indexOf("\n"); 
+		i = request.indexOf("\n");  	// Take end before return carriage
 		version = request.substring(0, i-1);
+
 
 
 		/* Extract headerlines */
 		while (true)
 		{
-			request = request.substring(i+1);
-			i = request.indexOf("\n");
+			request = request.substring(i+1); 	// Take begin of headerline after return carriage
+			i = request.indexOf("\n"); 			// Take end of headerline before return carriage
 
 			String headerline = request.substring(0, i-1);
-			if (headerline = "\r")
+			if (request.substring(i-2, i-1) = "\r") 	// If '\r' before return carriage, end of headerlines
+			{
+				int j = headerline.indexOf(":");
+				if (j != -1)
+					headerlines.put(headerline.substring(0, j-1), headerline.substring(j+2));	
 				break;
+			}
 
-			int j = headerline.indexOf(":");
-			headerlines.put(headerline.substring(0, j-1), headerline.substring(j+2));
+			int j = headerline.indexOf(":"); 	// Take before and after 
+			headerlines.put(headerline.substring(0, j-1), headerline.substring(j+2)); 
 		}
 
 
+
 		/* Extract entity body if there is one */
+		if (request.substr(i+1, i+2) != "\r\n")
+		{
+			validRequest = false;
+			return;
+		}
+
+
+		/* If body is empty */
+		if (i+3 >= request.length())
+			return;
+
+
+
+		/* If body contains arguments */
+		if (method == "GET")
+		{
+			validRequest = false;
+			return;
+		}
+
+
+		request.substring(i+3);
 		while (true)
 		{
-			// TODO
+			int k = args.indexOf("="); 		// Take end of argumentname before '='
+			if (k == -1)
+			{
+				validRequest = false;
+				return;
+			}
+
+			int l = args.indexOf("&"); 		// Take end of argumentvalue before '&'
+
+			if (l == -1) // If last argument of url
+			{
+				if (l+1 >= args.length()) 	// Empty argument
+					break;
+
+				arguments.put(args.substring(0, k-1), args.substring(k+1)); 	// Put argname and argvalue in map
+				break;
+			}
+
+			arguments.put(args.substring(0, k-1), args.substring(k+1, l-1)); 	// Put argname and argvalue in map
+			args = args.substring(l+1); 	// Go to next argument
 		}
 	}	
 }
